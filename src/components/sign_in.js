ee8f69575190437f8c614f6f0b6541a1dcf7bb39
camposss <ccamposs23@gmail.com>
2018-01-19 16:41:47 -0800

    completed sign in and sign up forms using redux form

--- src/components/sign_in.js
+++ src/components/sign_in.js
@@ -1,13 +1,55 @@
 import React, {Component} from 'react';
-
+import {Field, reduxForm} from 'redux-form';
+import { renderInput } from "../helpers";
 
 class SignIn extends Component {
+
+    handleSignIn(values){
+        console.log('values are: ', values);
+    }
     render(){
+        const {handleSubmit}= this.props;
         return(
             <div>
                 <h1 className="center-align">Sign In</h1>
+                <div className='row'>
+                    <div className="col s8 offset-s2">
+                        <div className='card'>
+                            <div className="card-content ">
+                                <form onSubmit={handleSubmit(this.handleSignIn.bind(this))}>
+                                    <Field name='email' type='text' placeholder='Enter email address' component={renderInput}/>
+                                    <Field name='password' type='password' placeholder='Enter password' component={renderInput}/>
+                                    <div className='right-align'>
+                                        <button className='btn green darken-1'>Sign In</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+function validate(values){
+    const error= {};
+
+    if(!values.email){
+        error.email= "Please enter an email"
+    }
+    if(!values.password){
+        error.password= "Please enter a password"
+    }
+    if(values.password!==values.confirmPassword){
+        error.confirmPassword = 'Passwords do not match!!!!'
+    }
+    return error;
+}
+SignIn= reduxForm({
+    form: 'sign-in',
+    validate: validate
+})(SignIn);
+
+
+
 export default SignIn;
